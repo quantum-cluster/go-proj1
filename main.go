31a478be626c771c7deb9f1d3cd64776eae86fb5
Nikhil Choudhary <nikhilchoudhary@Priyankas-MacBook-Air.local>
2026-03-19 01:05:35 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Starting process...")
+	startTime := time.Now()
+	// Your code starts
+	//
+	// Your code ends
+	fmt.Println("Ending process. Total time taken:", time.Since(startTime))
+}
